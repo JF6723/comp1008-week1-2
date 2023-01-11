096a39074371846a211a13629f2c4a92103781c4
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-11 10:28:25 -0500

    Added in modulus and displayed the word Java when the count is divisble by 20

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i=100; i>=0; i=i-5)
-            System.out.println("i="+i);
+        for (int i=0; i<=100; i=i+10)
+        {
+            //modulus is the remainder after doing integer division
+            //20 / 20 = 1 with a remainder of 0  Modulus is 0
+            //30 / 20 = 1 with a remainder of 10 Modulus is 10
+            System.out.printf("i=%3d %s %n",i,(i%20==0)?"Java":"");
+        }
     }
 }
